e3c42e3019c674b32aeb2f573304cea1d0fb58df
Bikram <bikram.aditya2099@gmail.com>
2019-11-17 16:16:20 +0530

    new website modification

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,261 +1,244 @@
-/** 
-  * Template Name: Varsity
-  * Version: 1.0  
-  * Template Scripts
-  * Author: MarkUps
-  * Author URI: http://www.markups.io/
+(function($) {
+  "use strict";
 
-  Custom JS
-  
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 30,
+    nav: true,
+    pagination: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 2
+      },
+      1000: {
+        items: 3
+      }
+    }
+  });
 
-  1. SEARCH FORM
-  2. ABOUT US VIDEO
-  2. TOP SLIDER
-  3. ABOUT US (SLICK SLIDER) 
-  4. LATEST COURSE SLIDER (SLICK SLIDER) 
-  5. TESTIMONIAL SLIDER (SLICK SLIDER)
-  6. COUNTER
-  7. RELATED ITEM SLIDER (SLICK SLIDER)
-  8. MIXIT FILTER (FOR GALLERY)
-  9. FANCYBOX (FOR PORTFOLIO POPUP VIEW)  
-  11. HOVER DROPDOWN MENU
-  12. SCROLL TOP BUTTON  
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    var box = $(".header-text").height();
+    var header = $("header").height();
 
-  
-**/
+    if (scroll >= box - header) {
+      $("header").addClass("background-header");
+    } else {
+      $("header").removeClass("background-header");
+    }
+  });
 
-jQuery(function($){
+  // Mobile menu dropdown
+  $(".submenu").on("click", function() {
+    var width = $(window).width();
+    if (width < 992) {
+      $(".submenu ul").toggleClass("active");
+    }
+  });
 
+  // Scroll animation init
+  window.sr = new scrollReveal();
 
-  /* ----------------------------------------------------------- */
-  /*  1. SEARCH FORM
-  /* ----------------------------------------------------------- */
-
-   jQuery('#mu-search-icon').on('click', function(event) {
-        event.preventDefault();
-        $('#mu-search').addClass('mu-search-open');
-        $('#mu-search form input[type="search"]').focus();
+  // Menu Dropdown Toggle
+  if ($(".menu-trigger").length) {
+    $(".menu-trigger").on("click", function() {
+      $(this).toggleClass("active");
+      $(".header-area .nav").slideToggle(200);
     });
-    
-    jQuery('.mu-search-close').on('click', function(event) {
-      $("#mu-search").removeClass('mu-search-open');
-    });
+  }
 
-/* ----------------------------------------------------------- */
-/*  2. ABOUT US VIDEO
-/* ----------------------------------------------------------- */
-    // WHEN CLICK PLAY BUTTON 
-    jQuery('#mu-abtus-video').on('click', function(event) {
-      event.preventDefault();
-      $('body').append("<div id='about-video-popup'><span id='mu-video-close' class='fa fa-close'></span><iframe id='mutube-video' name='mutube-video' frameborder='0' allowfullscreen></iframe></div>");        
-      $("#mutube-video").attr("src", $(this).attr("href"));
-    });         
-    // WHEN CLICK CLOSE BUTTON
-    $(document).on('click','#mu-video-close', function(event) {     
-      $(this).parent("div").fadeOut(1000);
-    });
-    // WHEN CLICK OVERLAY BACKGROUND
-    $(document).on('click','#about-video-popup', function(event) {
-      $(this).remove();
-    });
-  
-  /* ----------------------------------------------------------- */
-  /*  3. TOP SLIDER (SLICK SLIDER)
-  /* ----------------------------------------------------------- */    
-
-    jQuery('#mu-slider').slick({
-      dots: false,
-      infinite: true,
-      arrows: true,
-      speed: 500,     
-      autoplay: true,      
-      cssEase: 'linear'
-    });
-
-  /* ----------------------------------------------------------- */
-  /*  4. ABOUT US (SLICK SLIDER)
-  /* ----------------------------------------------------------- */      
-
-    jQuery('#mu-testimonial-slide').slick({
-      dots: true,
-      infinite: true,
-      arrows: false,
-      speed: 500,
-      autoplay: true,          
-      cssEase: 'linear'
-    });
-  
-
-  /* ----------------------------------------------------------- */
-  /*  5. LATEST COURSE SLIDER (SLICK SLIDER)
-  /* ----------------------------------------------------------- */    
-
-    jQuery('#mu-latest-course-slide').slick({
-      dots: true,
-      arrows: false,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 3,
-      slidesToScroll: 2,
-      autoplay: true,
-      autoplaySpeed: 2500,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
+  // Menu elevator animation
+  $("a[href*=\\#]:not([href=\\#])").on("click", function() {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        var width = $(window).width();
+        if (width < 991) {
+          $(".menu-trigger").removeClass("active");
+          $(".header-area .nav").slideUp(200);
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
+        $("html,body").animate(
+          {
+            scrollTop: target.offset().top - 80
+          },
+          700
+        );
+        return false;
+      }
+    }
+  });
+
+  $(document).ready(function() {
+    $(document).on("scroll", onScroll);
+
+    //smoothscroll
+    $('a[href^="#"]').on("click", function(e) {
+      e.preventDefault();
+      $(document).off("scroll");
+
+      $("a").each(function() {
+        $(this).removeClass("active");
+      });
+      $(this).addClass("active");
+
+      var target = this.hash,
+        menu = target;
+      var target = $(this.hash);
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: target.offset().top - 79
+          },
+          500,
+          "swing",
+          function() {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+          }
+        );
     });
+  });
 
-  /* ----------------------------------------------------------- */
-  /*  6. TESTIMONIAL SLIDER (SLICK SLIDER)
-  /* ----------------------------------------------------------- */    
+  function onScroll(event) {
+    var scrollPos = $(document).scrollTop();
+    $(".nav a").each(function() {
+      var currLink = $(this);
 
-    jQuery('.mu-testimonial-slider').slick({
-      dots: true,      
-      infinite: true,
-      arrows: false,
-      autoplay: true,
-      speed: 500,      
-      cssEase: 'linear'
-    });       
-
-  /* ----------------------------------------------------------- */
-  /*  7. COUNTER
-  /* ----------------------------------------------------------- */
-
-    jQuery('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
-
-
-  /* ----------------------------------------------------------- */
-  /*  8. RELATED ITEM SLIDER (SLICK SLIDER)
-  /* ----------------------------------------------------------- */    
-
-    jQuery('#mu-related-item-slide').slick({
-      dots: false,
-      arrows: true,
-      infinite: true,
-      speed: 300,
-      slidesToShow: 2,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 2500,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: false
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
+      try {
+        var refElement = $(currLink.attr("href"));
+        if (
+          refElement.position().top <= scrollPos &&
+          refElement.position().top + refElement.height() > scrollPos
+        ) {
+          $(".nav ul li a").removeClass("active");
+          currLink.addClass("active");
+        } else {
+          currLink.removeClass("active");
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-      ]
-    });
-
-  /* ----------------------------------------------------------- */
-  /*  9. MIXIT FILTER (FOR GALLERY) 
-  /* ----------------------------------------------------------- */  
-
-    jQuery(function(){
-      jQuery('#mixit-container').mixItUp();
-    });
-
-  /* ----------------------------------------------------------- */
-  /*  10. FANCYBOX (FOR PORTFOLIO POPUP VIEW) 
-  /* ----------------------------------------------------------- */ 
-      
-    jQuery(document).ready(function() {
-      jQuery(".fancybox").fancybox();
-    });
-  
-  /* ----------------------------------------------------------- */
-  /*  11. HOVER DROPDOWN MENU
-  /* ----------------------------------------------------------- */ 
-  
-  // for hover dropdown menu
-    jQuery('ul.nav li.dropdown').hover(function() {
-      jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(200);
-    }, function() {
-      jQuery(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(200);
-    });
-
-    
-  /* ----------------------------------------------------------- */
-  /*  12. SCROLL TOP BUTTON
-  /* ----------------------------------------------------------- */
-
-  //Check to see if the window is top if not then display button
-
-    jQuery(window).scroll(function(){
-      if (jQuery(this).scrollTop() > 300) {
-        jQuery('.scrollToTop').fadeIn();
-      } else {
-        jQuery('.scrollToTop').fadeOut();
+      } catch (e) {
+        // Ignore href='javascript:;'
       }
     });
-     
-    //Click event to scroll to top
+  }
 
-    jQuery('.scrollToTop').click(function(){
-      jQuery('html, body').animate({scrollTop : 0},800);
-      return false;
-    });  
-    $(document).ready(function() {
-      var today = new Date().toDateString();
-    $('.today').html(today);
+  const Accordion = {
+    settings: {
+      // Expand the first item by default
+      first_expanded: false,
+      // Allow items to be toggled independently
+      toggle: false
+    },
+
+    openAccordion: function(toggle, content) {
+      if (content.children.length) {
+        toggle.classList.add("is-open");
+        let final_height = Math.floor(content.children[0].offsetHeight);
+        content.style.height = final_height + "px";
+      }
+    },
+
+    closeAccordion: function(toggle, content) {
+      toggle.classList.remove("is-open");
+      content.style.height = 0;
+    },
+
+    init: function(el) {
+      const _this = this;
+
+      // Override default settings with classes
+      let is_first_expanded = _this.settings.first_expanded;
+      if (el.classList.contains("is-first-expanded")) is_first_expanded = true;
+      let is_toggle = _this.settings.toggle;
+      if (el.classList.contains("is-toggle")) is_toggle = true;
+
+      // Loop through the accordion's sections and set up the click behavior
+      const sections = el.getElementsByClassName("accordion");
+      const all_toggles = el.getElementsByClassName("accordion-head");
+      const all_contents = el.getElementsByClassName("accordion-body");
+      for (let i = 0; i < sections.length; i++) {
+        const section = sections[i];
+        const toggle = all_toggles[i];
+        const content = all_contents[i];
+
+        // Click behavior
+        toggle.addEventListener("click", function(e) {
+          if (!is_toggle) {
+            // Hide all content areas first
+            for (let a = 0; a < all_contents.length; a++) {
+              _this.closeAccordion(all_toggles[a], all_contents[a]);
+            }
+
+            // Expand the clicked item
+            _this.openAccordion(toggle, content);
+          } else {
+            // Toggle the clicked item
+            if (toggle.classList.contains("is-open")) {
+              _this.closeAccordion(toggle, content);
+            } else {
+              _this.openAccordion(toggle, content);
+            }
+          }
+        });
+
+        // Expand the first item
+        if (i === 0 && is_first_expanded) {
+          _this.openAccordion(toggle, content);
+        }
+      }
+    }
+  };
+
+  (function() {
+    // Initiate all instances on the page
+    const accordions = document.getElementsByClassName("accordions");
+    for (let i = 0; i < accordions.length; i++) {
+      Accordion.init(accordions[i]);
+    }
+  })();
+
+  // Home seperator
+  if ($(".home-seperator").length) {
+    $(".home-seperator .left-item, .home-seperator .right-item").imgfix();
+  }
+
+  // Home number counterup
+  if ($(".count-item").length) {
+    $(".count-item strong").counterUp({
+      delay: 10,
+      time: 1000
+    });
+  }
+
+  // Page loading animation
+  $(window).on("load", function() {
+    if ($(".cover").length) {
+      $(".cover").parallax({
+        imageSrc: $(".cover").data("image"),
+        zIndex: "1"
+      });
+    }
+
+    $("#preloader").animate(
+      {
+        opacity: "0"
+      },
+      600,
+      function() {
+        setTimeout(function() {
+          $("#preloader")
+            .css("visibility", "hidden")
+            .fadeOut();
+        }, 300);
+      }
+    );
   });
-
-});
-
-var app = angular.module("myApp", []);
-app.controller("myCtrl", function($scope,$http) {
-  $http.get("/assets/json/homepage.json")
-  .then(function(response) {
-   console.log(JSON.stringify(response));
-   $scope.pageHeader=response.data;
-  });
-});
-
+})(window.jQuery);
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,27 @@
 var app = angular.module('codernation', ['ngRoute','ui.bootstrap']);
 
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+      templateUrl : "/user/login.html"
+    })
+    .when("/dashboard", {
+      templateUrl : "/user/dashboard.html"
+    })
+    .when("/register", {
+        templateUrl : "/user/register.html"
+      })
+      .when("/error", {
+        templateUrl : "/500.html"
+      })
+      .when("/login", {
+        templateUrl : "/user/login.html"
+      })
+   
+  });
 app.service('urlService', function() {
     this.host = function () {
-      return "http://localhost:8081/";
+      return "https://myzkdddw4f.execute-api.us-west-2.amazonaws.com/dev/";
     }
     this.registerUrl=function(){
         return "register";
@@ -10,6 +29,9 @@ app.service('urlService', function() {
 
     this.loginUrl=function(){
         return "authenticate";
+    }
+    this.userDetails=function(){
+        return "getuserdetails";
     }
 
   });
@@ -26,20 +48,133 @@ app.factory('userService',['$rootScope',function($rootScope){
   
     setToken : function (token) {
       user.token = token;
-      $rootScope.$broadcast("updates");
     }
   
   }
   }]);
 
+  app.controller('userController', function($rootScope,$scope,$http,urlService,$uibModal,userService,$window) {
 
+    require(['c3', 'jquery'], function(c3, $) {
+        $(document).ready(function(){
+          var chart = c3.generate({
+            bindto: '#chart-donut', // id of chart wrapper
+            data: {
+              columns: [
+                  // each columns data
+                ['data1', 80],
+                ['data2', 20]
+              ],
+              type: 'donut', // default type of chart
+              colors: {
+                'data1': tabler.colors["blue-darker"],
+                'data2': tabler.colors["blue-light"]
+              },
+              names: {
+                  // name of each serie
+                'data1': 'Present',
+                'data2': 'Absent'
+              }
+            },
+            axis: {
+            },
+            legend: {
+                      show: false, //hide legend
+            },
+            padding: {
+              bottom: 0,
+              top: 0
+            },
+          });
+        });
+      });
+
+      require(['c3', 'jquery'], function(c3, $) {
+        $(document).ready(function(){
+          var chart = c3.generate({
+            bindto: '#chart-pie', // id of chart wrapper
+            data: {
+              columns: [
+                  // each columns data
+                ['data1', 60],
+                ['data2', 20],
+                ['data3', 10],
+                ['data4', 10]
+              ],
+              type: 'pie', // default type of chart
+              colors: {
+                'data1': tabler.colors["blue-darker"],
+                'data2': tabler.colors["blue"],
+                'data3': tabler.colors["blue-light"],
+                'data4': tabler.colors["blue-lighter"]
+              },
+              names: {
+                  // name of each serie
+                'data1': 'Python',
+                'data2': 'Groovy On Grails',
+                'data3': 'Oracle',
+                'data4': 'Jenkins'
+              }
+            },
+            axis: {
+            },
+            legend: {
+                      show: false, //hide legend
+            },
+            padding: {
+              bottom: 0,
+              top: 0
+            },
+          });
+        });
+      });
+
+    console.log(sessionStorage.token);
+   if(sessionStorage.token==undefined || sessionStorage.token=="undefined"){
+    $window.location.href ="/user/";
+   }
+   else{
+    var requrl=urlService.host()+urlService.userDetails();
+    var token=JSON.parse(sessionStorage.token);
+    $http({
+        url: requrl,
+        method: "GET",
+        headers: {
+            'token': 'Bearer ' +token.token,
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(function(response) {
+       if(response.data.status!="FAIL"){
+       $scope.userDetails=response.data;
+       console.log($scope.userDetails);
+        }
+    }, 
+    function(response) { // optional
+           console.log(response);
+           $window.location.href="#!error";
+    });
+   }
+
+  
+    $scope.logout=function(){
+    sessionStorage.token=undefined;
+    $window.location.href="/user/";
+   }
+
+});  
 
 app.controller('loginController', function($scope,$http,urlService,$uibModal,userService,$window) {
-
-$scope.signIn=function(){
+    $("#spinner").hide();
+    $scope.formmodel={};
+    $scope.validuser=true;
+    $scope.signIn=function(){
+        $("#spinner").show();
+    $scope.validuser=true;
     var json={};
-    json.username=$scope.username;
-    json.password=$scope.password;
+    json.username=$scope.formmodel.username;
+    json.password=$scope.formmodel.password;
     var requrl=urlService.host()+urlService.loginUrl();
     $http({
         url: requrl,
@@ -47,14 +182,22 @@ $scope.signIn=function(){
         data: json
     })
     .then(function(response) {
-      if(response.data.status!="FAIL"){
+        $("#spinner").hide();
+        if(response.data.status!="FAIL"){
+            $scope.validuser=true;
         userService.setToken(response.data);
+        sessionStorage.token=JSON.stringify(response.data);
            console.log(response);
-           $window.location.href = "/corner";
-      }
+           $window.location.href = "#!dashboard";
+        }
+        else{
+            $scope.validuser=false;
+        }
     }, 
     function(response) { // optional
+        $("#spinner").hide();
            console.log(response);
+           $window.location.href="#!error";
     });
 
 
@@ -63,6 +206,9 @@ $scope.signIn=function(){
 });   
 app.controller('registrationController', function($scope,$http,urlService,$uibModal) {
   
+        $("#spinner").hide();
+  
+   
   $scope.createAccount=function(){
     
     $scope.json={};
